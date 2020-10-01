9038a419cf7faa4c5e3083aa2ad0dcc720a1215e
abdul kareem <as.abdulkareem@gmail.com>
2020-10-01 19:51:51 +0530

    Create gh-pages branch with static content

---
+++ precache-manifest.8d8278b3fdd818149dafcf48c4f237de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b006c213621849cf1fb77f57acc9451a",
+    "url": "/webdevelopement/index.html"
+  },
+  {
+    "revision": "6fed313d3b44db35226d",
+    "url": "/webdevelopement/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "47e5c09e2c53ea43c962",
+    "url": "/webdevelopement/static/js/2.0952b43c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webdevelopement/static/js/2.0952b43c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fed313d3b44db35226d",
+    "url": "/webdevelopement/static/js/main.f4263bb7.chunk.js"
+  },
+  {
+    "revision": "4672cf6fd10ba70942d7",
+    "url": "/webdevelopement/static/js/runtime-main.52a5032c.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/webdevelopement/static/media/logo.ee7cd8ed.svg"
+  }
+]);
